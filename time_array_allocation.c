6ba71a30936b36e3b937b9d9b7addfeace1e2a24
Christos Argyropoulos <chrisarg@gmail.com>
2024-08-14 21:26:23 -0600

    Times array allocation in C/Perl
    Compares time to allocate an array of bytes of a given length in pure C, and 3 methods in Perl: inline C, a string and packing an array constructed on the fly

---
+++ time_array_allocation.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+char* allocate_and_initialize_array(size_t length, char initial_value) {
+    // Allocate memory for the array
+    char* array = (char*)malloc(length * sizeof(char));
+    if (array == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+
+    // Initialize each element with the initial_value
+    memset(array, initial_value, length);
+
+    return array;
+}
+
+double time_allocation_and_initialization(size_t length, char initial_value) {
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+    char* array = allocate_and_initialize_array(length, initial_value);
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    free(array); // Free the allocated memory
+
+    return cpu_time_used;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <length> <initial_value>\n", argv[0]);
+        return 1;
+    }
+
+    size_t length = strtoull(argv[1], NULL, 10);
+    char initial_value = argv[2][0];
+
+    double time_taken = time_allocation_and_initialization(length, initial_value);
+    printf("Time taken to allocate and initialize array: %f seconds\n", time_taken);
+    printf("Initializes per second: %f\n", 1/time_taken);
+
+    return 0;
+}
+
+/*
+Compilation command:
+gcc -o time_array_allocation time_array_allocation.c -std=c99
+
+Example invocation:
+./time_array_allocation 10000000 A
+*/
